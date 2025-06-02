c53713bb9731aa88d3d7ca6ea1a8e62f5cffa240
Sufyan <sufyanmq06@gmail.com>
2025-06-02 18:23:58 +0200

    Initial commit for Proyecto 2 - Portfolio

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,16 +3,16 @@ export const projects = [
     title: "Personal Portfolio",
     description:
       "A single-page portfolio built with Vite to showcase my web development projects, using responsive design and modern UI.",
-    image: "./public/img-projects/Portfolio.png",
+    image: "./public/icons/img-projects/Portfolio.png",
     tech: ["HTML5", "CSS3", "JavaScript", "Vite"],
-    github: "https://github.com/SMQ56/portfolio", // provisional, se actualizará tras crear la repo
-    link: "https://portfolio-smq56.vercel.app", // provisional, se actualizará tras el deploy
+    github: "https://github.com/SMQ56/Proyecto-2-Portfolio.git",
+    link: "https://proyecto-2-portfolio.vercel.app",
   },
   {
     title: "Dynamic Product Gallery",
     description:
       "An interactive video game store built with JavaScript, featuring dynamic product rendering, filters, search, and responsive design.",
-    image: "./public/img-projects/dynamic-gallery.png",
+    image: "./public/icons/img-projects/dynamic-gallery.png",
     tech: ["HTML5", "CSS3", "JavaScript"],
     github: "https://github.com/SMQ56/DOM-Practice-Dynamic-Gallery.git",
     link: "https://dom-practice-dynamic-gallery.vercel.app/",
@@ -21,7 +21,7 @@ export const projects = [
     title: "Epic Games Store Clone",
     description:
       "A static replica of the Epic Games Store homepage with modern layout and styling using HTML5 and CSS3.",
-    image: "./public/img-projects/EpicGames.png",
+    image: "./public/icons/img-projects/EpicGames.png",
     tech: ["HTML5", "CSS3"],
     github: "https://github.com/SMQ56/Proyecto_1.git",
     link: "https://proyecto-1epicgames.vercel.app",
@@ -30,7 +30,7 @@ export const projects = [
     title: "Landing Page",
     description:
       "A modern and responsive landing page for a fictional game studio, featuring a full-screen video background, responsive design using Flexbox, and multiple sections showcasing games, services, and a newsletter signup form.",
-    image: "./public/img-projects/flexbox.png",
+    image: "./public/icons/img-projects/flexbox.png",
     tech: ["HTML5", "CSS3"],
     github: "https://github.com/SMQ56/Flexbox.git",
     link: "https://flexbox-five-neon.vercel.app",
@@ -39,7 +39,7 @@ export const projects = [
     title: "Rolls-Royce Landing Page",
     description:
       "A static landing page dedicated to Rolls-Royce, showcasing the brand's history, iconic models, and luxury appeal. Built with semantic HTML5 and styled using modern CSS3 techniques.",
-    image: "./public/img-projects/Landing-Page.png",
+    image: "./public/icons/img-projects/Landing-Page.png",
     tech: ["HTML5", "CSS3"],
     github: "https://github.com/SMQ56/Entrega_Posicionamiento.git",
     link: "",
